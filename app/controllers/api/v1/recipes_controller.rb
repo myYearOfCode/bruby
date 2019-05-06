f37714ff129d2293636fe27354a791e9fabcbf05
Ross <Ross@myyearofcode.com>
2019-05-06 10:12:37 -0400

    backend code for deleting a recipe

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -27,7 +27,37 @@ class Api::V1::RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user
+      if Recipe.find(destroy_params[:id]).delete
+        render json: {
+          recipes: current_user.recipes,
+          error: nil
+        }
+      else
+        render json: {
+          recipes: nil,
+          error: Recipe.errors.full_messages.join(", ")
+        }
+      end
+    else
+      render json: {
+        recipes: nil,
+        error: "you must be signed in to delete a recipe"
+      }
+    end
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        recipes: nil,
+        error: "no recipe found"
+      }
+  end
+
   private
+  def destroy_params
+    params.require(:recipe).permit(:id)
+  end
+
   def recipe_params
     params.require(:recipe).permit(:name, :s1Temp, :s1Time, :s2Temp, :s2Time, :s3Temp, :s3Time, :s4Temp, :s4Time, :s5Temp, :s5Time, :s6Temp, :s6Time, :s7Temp, :s7Time, :s8Temp, :s8Time, :s9Temp, :s9Time, :s10Temp, :s10Time)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :recipes, only: [:index, :create]
+      resources :recipes, only: [:index, :create, :destroy]
       resources :users, only: [:index]
     end
   end
