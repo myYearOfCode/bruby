eefb6692e3add777f768749e3f3688b40ef0aeca
Ross <Ross@myyearofcode.com>
2019-05-05 22:25:14 -0400

    moving state and logic to the dashboard.

--- app/javascript/react/components/RecipeForm.js
+++ app/javascript/react/components/RecipeForm.js
@@ -4,32 +4,21 @@ import RecipeStep from './RecipeStep'
 class RecipeForm extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      recipeName: ''
-    }
-    this.handlerFunction = this.handlerFunction.bind(this)
-  }
-
-  handlerFunction(event) {
-    this.setState({ [event.target.name]: event.target.value })
   }
 
   render () {
-
     let recipeStepNames = ["Preparing to Brew","Heating","Dough In","Mash 1","Mash 2","Mash Out","Hops 1","Hops 2","Hops 3","Hops 4"]
-
     let recipeSteps = () => {
       return recipeStepNames.map((stepName, index) => {
         return (
-          <div>
-            <RecipeStep
-              stepName={stepName}
-              handlerFunction={this.handlerFunction}
-              step_num={index+1}
-              time_val={this.state[`s${index+1}time`]}
-              temp_val={5}
-            />
-          </div>
+          <RecipeStep
+            stepName={stepName}
+            recipeOnChangeHandler={this.props.recipeOnChangeHandler}
+            step_num={index+1}
+            time_val={this.props.recipe[`s${index+1}Time`] || 0}
+            temp_val={this.props.recipe[`s${index+1}Temp`] || 0}
+            key={`step${index+1}`}
+          />
         )
       })
     }
@@ -37,38 +26,35 @@ class RecipeForm extends Component {
     return (
       <div>
         <div id="form_container">
+          <form onSubmit={this.props.createRecipe}>
+            <div className="Recipe Builder">
             <h1>
-              Bruby Recipe Builder
+            Bruby Recipe Builder
             </h1>
-            <form method="post" action="/setRecipe">
-              <div className="MyMatic Recipe Builder">
               <ul>
                 <div className="step_wrapper">
                   <label className="label" htmlFor="recipeName">Recipe Name </label>
                   <input
-                    id="recipeName"
-                    name="recipeName"
+                    id="name"
+                    name="name"
                     className="element text medium"
                     type="text"
-                    onChange={this.handlerFunction}
+                    value={this.props.recipe.name || ""}
+                    onChange={this.props.recipeOnChangeHandler}
                   />
                 </div>
                 {recipeSteps()}
               </ul>
-              <input type="hidden" name="form_id" value="53587" />
-
-
-          </div>
-          <div className="button-group">
-            <button className="button" onClick={this.handleClearForm}>Clear</button>
-            <button className="button" type="submit" value="Submit"> Submit </button>
-          </div>
-        </form>
+            </div>
+            <div className="button-group">
+              <button className="button" onClick={this.props.clearForm} value="Clear">Clear</button>
+              <button className="button" type="submit" value="Submit"> Submit </button>
+            </div>
+          </form>
         </div>
       </div>
     )
   }
-
 }
 
 export default RecipeForm
