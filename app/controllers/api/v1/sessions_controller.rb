bec86f6483c5e57949c6b26631f29aab19a19cb8
Ross <Ross@myyearofcode.com>
2019-05-11 22:30:51 -0400

    remove pry, dummy.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,7 +13,6 @@ class Api::V1::SessionsController < ApplicationController
       end
       render json: response
     else
-      binding.pry
       render json: {error: "you are not signed in"}
     end
   end
