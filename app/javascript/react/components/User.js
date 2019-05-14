12d97c7e1c774a70b60ba5ad30c6021c10fd6352
Ross <Ross@myyearofcode.com>
2019-05-13 23:48:24 -0400

    looking better, but still needs to pull in live data.

--- app/javascript/react/components/User.js
+++ app/javascript/react/components/User.js
@@ -8,8 +8,6 @@ class User extends Component {
   }
 
   render () {
-
-
     return(
       <div className="userInfoBody">
         <div className="userInfoWrapper">
@@ -20,52 +18,61 @@ class User extends Component {
           </div>
           <div>
             <div className="welcomeBackText">
-              Welcome back{`, ${this.props.user.username}!` || "!"}
+              Welcome back{`, ${this.props.user.username}.` || "."}
             </div>
-            <div className="statsContainer">
-              <div className="statsWrapper">
-                <div className="numBrewsWrapper">
-                  <div className="numBrews big">
-                    10
-                  </div>
-                  <div className="numBrews description">
-                    Brews
+
+            <div className="userContainer">
+              <div className="statsContainer">
+                <div className="statsWrapper">
+                  <div className="numBrewsWrapper">
+                    <div className="numBrews big">
+                      10
                     </div>
-                </div>
-                <div className="numRecipesWrapper">
-                  <div className="numRecipes big">
-                    8
+                    <div className="numBrews description">
+                      Brews
+                      </div>
                   </div>
-                  <div className="numRecipes description">
-                    recipes
+                  <div className="numRecipesWrapper">
+                    <div className="numRecipes big">
+                      8
+                    </div>
+                    <div className="numRecipes description">
+                      recipes
+                    </div>
+                  </div>
+                </div>
+                <div className="statsWrapper">
+                  <div className="numHoursWrapper">
+                    <div className="numHours big">
+                      10
+                    </div>
+                    <div className="numBrews description">
+                      Hours Brewing
+                    </div>
+                  </div>
+                  <div className="numRecipesWrapper">
+                    <div className="numRecipes big">
+                      8
+                    </div>
+                    <div className="numRecipes description">
+                      Liters
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="statsWrapper">
-                <div className="numHoursWrapper">
-                  <div className="numHours big">
-                    10
-                  </div>
-                  <div className="numBrews description">
-                    Hours Brewing
-                  </div>
+              <div className="recapContainer">
+                <div className="brewingRecap">
+                  Brewed Last:
                 </div>
-                <div className="numRecipesWrapper">
-                  <div className="numRecipes big">
-                    8
-                  </div>
-                  <div className="numRecipes description">
-                    Liters
-                  </div>
+                <div className="brewingRecap beerName">
+                  Chocolate Stout
                 </div>
-              </div>
-            </div>
-            <div className="numSomethingWrapper">
-              <div className="numSomething big">
-                8
-              </div>
-              <div className="numSomething description">
-                Liters
+                <div className="brewingRecap">
+                  Brewing Next:
+                </div>
+                <div className="brewingRecap beerName">
+                  NEIPA
+                </div>
               </div>
             </div>
           </div>
