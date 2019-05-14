a74831375a084f780b1bb7e72aace62938a54896
Ross <Ross@myyearofcode.com>
2019-05-13 23:06:07 -0400

    committing to try fitText

--- app/javascript/react/components/User.js
+++ app/javascript/react/components/User.js
@@ -22,37 +22,41 @@ class User extends Component {
             <div className="welcomeBackText">
               Welcome back{`, ${this.props.user.username}!` || "!"}
             </div>
-            <div className="statsWrapper">
-              <div className="numBrewsWrapper">
-                <div className="numBrews big">
-                  10
-                </div>
-                <div className="numBrews description">
-                  Brews
+            <div className="statsContainer">
+              <div className="statsWrapper">
+                <div className="numBrewsWrapper">
+                  <div className="numBrews big">
+                    10
                   </div>
-              </div>
-              <div className="numRecipesWrapper">
-                <div className="numRecipes big">
-                  8
+                  <div className="numBrews description">
+                    Brews
+                    </div>
                 </div>
-                <div className="numRecipes description">
-                  recipes
-                </div>
-              </div>
-              <div className="numHoursWrapper">
-                <div className="numHours big">
-                  10
-                </div>
-                <div className="numBrews description">
-                  Hours Brewing
+                <div className="numRecipesWrapper">
+                  <div className="numRecipes big">
+                    8
+                  </div>
+                  <div className="numRecipes description">
+                    recipes
+                  </div>
                 </div>
               </div>
-              <div className="numRecipesWrapper">
-                <div className="numRecipes big">
-                  8
+              <div className="statsWrapper">
+                <div className="numHoursWrapper">
+                  <div className="numHours big">
+                    10
+                  </div>
+                  <div className="numBrews description">
+                    Hours Brewing
+                  </div>
                 </div>
-                <div className="numRecipes description">
-                  Liters
+                <div className="numRecipesWrapper">
+                  <div className="numRecipes big">
+                    8
+                  </div>
+                  <div className="numRecipes description">
+                    Liters
+                  </div>
                 </div>
               </div>
             </div>
