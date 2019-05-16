bafddcae48fbf8c3b374cc837da2a61d93485e97
Ross <Ross@myyearofcode.com>
2019-05-15 21:59:51 -0400

    cleaning up commented code

--- spec/javascript/sampleComponentTest.js
+++ spec/javascript/sampleComponentTest.js
@@ -1,17 +1,18 @@
-// import Sample from '../../app/javascript/react/components/Sample'
-//
-// describe('Sample react component', () => {
-//   let wrapper;
-//
-//   beforeEach(() => {
-//     wrapper = mount(
-//       <Sample />
-//     )
-//   })
-//
-//   it('should mount', () => {
-//
-//     expect(wrapper.find('h3')).toBePresent();
-//     expect(wrapper.find('h3').text()).toBe('This Is A React Component.')
-//   });
-// });
+import Header from '../../app/javascript/react/containers/Header'
+
+describe('Header', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <Header
+      />
+    )
+  })
+
+  it('should mount', () => {
+debugger
+    expect(wrapper.find('h3')).toBePresent();
+    expect(wrapper.find('h3').text()).toBe('This Is A React Component.')
+  });
+});
